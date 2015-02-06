14890f99b53dcc468f1e8f83b7267defe76a8257
limaoxuan <563157030@qq.com>
2015-02-06 16:34:55 +0800

    init project

---
+++ MXAlertView/MXAlertView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MXAlertView
+//
+//  Created by 李茂轩 on 15/2/6.
+//  Copyright (c) 2015年 lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
